5202fb90be52cec666f1e1a949377f46922abbfd
Cody Gray <cody_gray@acellent.com>
2021-12-05 01:59:28 -0800

    Add built-in support for testing rooms.

--- src/main/java/org/sobotics/userstalker/UserStalker.java
+++ src/main/java/org/sobotics/userstalker/UserStalker.java
@@ -16,8 +16,18 @@ import org.sobotics.userstalker.ChatBot;
 
 public class UserStalker
 {
+   private static final String LOGIN_PROPERTIES_FILE = "./properties/login.properties";
+   private static final String SO_PROPERTIES_FILE    = "./properties/StackOverflow.properties";
+   private static final String SE_PROPERTIES_FILE    = "./properties/StackExchange.properties";
+
+   private static final Logger LOGGER = LoggerFactory.getLogger(UserStalker.class);
+
    public static String VERSION;
-      static
+
+
+   public static void main(String[] args) throws IOException
+   {
+      try
       {
          // Attempt to load version number from properties.
          try
@@ -30,19 +40,14 @@ public class UserStalker
          {
             VERSION = "<unknown>";
          }
-      }
 
-   private static final String LOGIN_PROPERTIES_FILE = "./properties/login.properties";
-   private static final String SO_PROPERTIES_FILE    = "./properties/StackOverflow.properties";
-   private static final String SE_PROPERTIES_FILE    = "./properties/StackExchange.properties";
+         // Determine if should run in testing mode. If so, append a note to the version number.
+         boolean testMode = ((args != null) && (args.length > 0) && (args[0].equals("test")));
+         if (testMode)
+         {
+            VERSION += " [TEST]";
+         }
 
-   private static final Logger LOGGER = LoggerFactory.getLogger(UserStalker.class);
-
-
-   public static void main(String[] args) throws IOException
-   {
-      try
-      {
          LOGGER.info("Starting User Stalker v" + VERSION + "...");
 
          // Load "login" properties file.
@@ -59,7 +64,7 @@ public class UserStalker
 
          // Load "StackOverflow" properties file.
          boolean joinSO   = true;
-         int     soRoomID = 239107;
+         int     soRoomID = -1;
          try
          {
             // Attempt to load Stack Overflow properties file.
@@ -74,16 +79,8 @@ public class UserStalker
                throw ex;
             }
 
-            // Attempt to load the room number from the properties file.
-            // If this fails or it is not available, fall back to the default room number.
-            try
-            {
-               soRoomID = Integer.parseInt(propSO.getProperty("roomID"));
-            }
-            catch (NumberFormatException ex)
-            {
-               LOGGER.warn("Missing or invalid value for \"roomID\" property in \"StackOverflow\" property file; will use default room " + soRoomID + ".");
-            }
+            // Attempt to load and parse the room number from the properties file.
+            soRoomID = Integer.parseInt(propSO.getProperty("roomID" + (testMode ? "_test" : "")));
          }
          catch (Exception ex)
          {
@@ -95,9 +92,9 @@ public class UserStalker
 
          // Load "StackExchange" properties file.
          boolean      joinSE          = true;
-         int          seRoomID        = 59667;
          List<String> seSites         = null;
          List<String> nonEnglishSites = null;
+         int          seRoomID        = -1;
          try
          {
             // Attempt to load Stack Exchange properties file.
@@ -110,17 +107,6 @@ public class UserStalker
             {
                LOGGER.warn("Failed to open \"StackExchange\" property file: \"" + SE_PROPERTIES_FILE + "\".");
                throw ex;
-            }
-
-            // Attempt to load the room number from the properties file.
-            // If this fails or it is not available, fall back to the default room number.
-            try
-            {
-               seRoomID = Integer.parseInt(propSE.getProperty("roomID"));
-            }
-            catch (NumberFormatException ex)
-            {
-               LOGGER.warn("Missing or invalid value for \"roomID\" property in \"StackExchange\" property file; will use default room " + seRoomID + ".");
             }
 
             // Attempt to load the list of sites from the properties file.
@@ -150,6 +136,9 @@ public class UserStalker
             {
                LOGGER.warn("Missing or invalid value for \"nonEnglish\" property in \"StackExchange\" property file; will not exclude any sites from non-Latin character checks.");
             }
+
+            // Attempt to load and parse the room number from the properties file.
+            seRoomID = Integer.parseInt(propSE.getProperty("roomID" + (testMode ? "_test" : "")));
          }
          catch (Exception ex)
          {
@@ -165,11 +154,11 @@ public class UserStalker
                                    propLogin.getProperty("password"));
 
          // Join the appropriate rooms.
-         if (joinSO)
+         if (joinSO && (soRoomID != -1))
          {
             bot.JoinSO(soRoomID);
          }
-         if (joinSE)
+         if (joinSE && (seRoomID != -1))
          {
             bot.JoinSE(seRoomID);
          }
